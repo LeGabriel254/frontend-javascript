5fa1824f5282d5d8cc73d90636858af472f02abe
Leon Gabriel <g73346920@gmail.com>
2024-12-17 13:41:55 +0300

    modified

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,7 +6,7 @@ interface Teacher{
   contract:false
  }
  
-const teacher3: Teacher = {
+const teacher: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
   lastName: 'Doe',
@@ -24,7 +24,7 @@ interface Directors{
   numberOfReports:number
 }
 
-const director1: Directors = {
+const director: Directors = {
   firstName: 'John',
   lastName: 'Doe',
   location: 'London',
@@ -33,55 +33,10 @@ const director1: Directors = {
 };
 console.log(director1);
 
-// Define the interface
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+{
+  const printTeacher = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+  };
+
+  console.log(printTeacher("John", "Doe")); // Output: J. Doe
 }
-
-// Implemented the function
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName.charAt(0)}. ${lastName}`;
-};
-
-// Example usage
-console.log(printTeacher("John", "Doe"));
-
-// Interface for the constructor
-interface StudentConstructor {
-  new (firstName: string, lastName: string): StudentInterface;
-}
-
-// Interface for the class methods
-interface StudentInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-// Class implementing the interfaces
-class Student implements StudentInterface {
-  private firstName: string;
-  private lastName: string;
-
-
-  // Constructor that initializes firstName and lastName
-  constructor(firstName: string, lastName: string) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-  }
-
-  // Method that returns the string "Currently working"
-  workOnHomework(): string {
-    return "Currently working";
-  }
-  // Method that returns the firstName of the student
-  displayName(): string {
-    return this.firstName;
-  }
-}
-
-// Example usage
-const student: StudentInterface = new Student("John", "Doe");
-
-console.log(student.workOnHomework());
-console.log(student.displayName());    
-
