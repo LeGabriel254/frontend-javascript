60dedff974c4555395315cbbfa294b5b85317b7d
Leon Gabriel <g73346920@gmail.com>
2024-12-17 13:56:30 +0300

    Initial commits

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,7 +14,7 @@ const teacher: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
+console.log(teacher);
 
 interface Directors{
   firstName:string,
@@ -31,12 +31,46 @@ const director: Directors = {
   fullTimeEmployee: true,
   numberOfReports: 17,
 };
-console.log(director1);
+console.log(director);
 
 {
   const printTeacher = (firstName: string, lastName: string): string => {
     return `${firstName.charAt(0)}. ${lastName}`;
   };
 
-  console.log(printTeacher("John", "Doe")); // Output: J. Doe
+  console.log(printTeacher("John", "Doe"));
 }
+
+// Interface to describe the constructor
+interface IStudentConstructor {
+  firstName: string;
+  lastName: string;
+}
+
+// Interface to describe the StudentClass
+interface IStudent {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementation
+class StudentClass implements IStudent {
+  firstName: string;
+  lastName: string;
+
+  constructor({ firstName, lastName }: IStudentConstructor) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
