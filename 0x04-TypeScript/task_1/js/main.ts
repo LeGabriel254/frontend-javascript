9e1a247501140ee483200ec846ccb43b3222d874
Leon Gabriel <g73346920@gmail.com>
2024-12-17 13:22:57 +0300

    Chored:Modified

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -46,3 +46,42 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
 // Example usage
 console.log(printTeacher("John", "Doe"));
 
+// Interface for the constructor
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+// Interface for the class methods
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+// Class implementing the interfaces
+class Student implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+
+  // Constructor that initializes firstName and lastName
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // Method that returns the string "Currently working"
+  workOnHomework(): string {
+    return "Currently working";
+  }
+  // Method that returns the firstName of the student
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+// Example usage
+const student: StudentInterface = new Student("John", "Doe");
+
+console.log(student.workOnHomework());
+console.log(student.displayName());    
+
